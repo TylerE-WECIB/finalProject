e326911aad2fd5252f4f293ac8ff5b94df83f782
Tytaneum <145382438+TylerE-WECIB@users.noreply.github.com>
2024-12-02 08:31:57 -0500

    mas progreso

---
+++ projectJS.js
@@ -0,0 +1,5 @@
+//setting up elements
+let planner_form = document.getElementById("meal_plan_form")
+let clear_button = document.getElementById("clear_form")
+let print_button = document.getElementById("print_form")
+let generate_button = document.getElementById("generate")
