7e0255ce4274c02573ffdf5b15f2b308ef5f4dd5
Tytaneum <145382438+TylerE-WECIB@users.noreply.github.com>
2024-12-02 09:58:26 -0500

    progress again (regex isn't working)

--- projectJS.js
+++ projectJS.js
@@ -2,7 +2,15 @@
 let planner_form = document.getElementById("meal_plan_form")
 let clear_button = document.getElementById("clear_form")
 let print_button = document.getElementById("print_form")
+let download_button = document.getElementById("download_form")
 let generate_button = document.getElementById("generate")
+
+//input fields (surprise tool that will help us later)
+let breakfast = document.getElementById("breakfast")
+let snack1 = document.getElementById("snack1")
+let lunch = document.getElementById("lunch")
+let snack2 = document.getElementById("snack2")
+let dinner = document.getElementById("dinner")
 
 //clear button functionality. clears all input field values
 clear_button.addEventListener('click',function(){
@@ -12,22 +20,18 @@ clear_button.addEventListener('click',function(){
     }
 })
 
-//generates the website upon pressing the generate planner button
-generate_button.addEventListener('click', function(){
+print_button.addEventListener('click', function(){
+
+})
+
+download_button.addEventListener('click', function(){
     
-    let email_field = document.getElementById("email")
-    //tests if email field is syntactically correct before submitting
-    //the abc will be replaced with a regex test. i need to figure out how to do regex emails
-    if(email_field.value == "abc"){
-        console.log("you can submit now")
-        planner_form.submit()
+})
 
-        //build the website here
-        buildPlanner()
-
-    }else{
-        alert("NO")
-    }
+//generates the website upon pressing the generate planner button
+planner_form.addEventListener('submit', function(){
+    //create the planner website when the form gets submitted
+    buildPlanner()
 })
 
 function buildPlanner(){
