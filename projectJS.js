06a5956ad5932693505ceebfff53e6ea49a10434
Tytaneum <145382438+TylerE-WECIB@users.noreply.github.com>
2024-12-03 07:50:27 -0500

    new window functionality almost working

--- projectJS.js
+++ projectJS.js
@@ -36,7 +36,73 @@ planner_form.addEventListener('submit', function(){
 
 function buildPlanner(){
     //creates the planner website
-    let table_text = ""
+    let table_text = `
+    <table border = 1>
+        <tr>
+            <th></th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+            <th>Sunday</th>
+        </tr>
+        <tr id="breakfast">
+            <th>Breakfast</th>
+            <td>${breakfast.value}</td>
+            <td>${breakfast.value}</td>
+            <td>${breakfast.value}</td>
+            <td>${breakfast.value}</td>
+            <td>${breakfast.value}</td>
+            <td>${breakfast.value}</td>
+            <td>${breakfast.value}</td>
+        </tr>
+        <tr id="snack 1">
+            <th>Snack 1</th>
+            <td>${snack1.value}</td>
+            <td>${snack1.value}</td>
+            <td>${snack1.value}</td>
+            <td>${snack1.value}</td>
+            <td>${snack1.value}</td>
+            <td>${snack1.value}</td>
+            <td>${snack1.value}</td>
+        </tr>
+        <tr id="lunch">
+            <th>Lunch</th>
+            <td>${lunch.value}</td>
+            <td>${lunch.value}</td>
+            <td>${lunch.value}</td>
+            <td>${lunch.value}</td>
+            <td>${lunch.value}</td>
+            <td>${lunch.value}</td>
+            <td>${lunch.value}</td>
+        </tr>
+        <tr id="snack 2">
+            <th>Snack 2</th>
+            <td>${snack2.value}</td>
+            <td>${snack2.value}</td>
+            <td>${snack2.value}</td>
+            <td>${snack2.value}</td>
+            <td>${snack2.value}</td>
+            <td>${snack2.value}</td>
+            <td>${snack2.value}</td>
+        </tr>
+        <tr id="dinner">
+            <th>Dinner</th>
+            <td>${dinner.value}</td>
+            <td>${dinner.value}</td>
+            <td>${dinner.value}</td>
+            <td>${dinner.value}</td>
+            <td>${dinner.value}</td>
+            <td>${dinner.value}</td>
+            <td>${dinner.value}</td>
+        </tr>
+    </table>
+    `
     new_planner = window.open('about:blank','This Week\'s Meal Plan','width=700,height=800')
     new_planner.document.write(`hello world<br>${table_text}`)
 }
+
+// [A-Za-z0-9._-]+@+[A-Za-z0-9-]+\.+[A-Za-z0-9]{2,}
+//regex that isn't working
